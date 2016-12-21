e75bfbd38ab54fc815dda7e2ae44dca63db52b4a
Lody Borgers <hi@lodybo.nl>
2016-12-21 13:23:34 +0100

    Added js classes to the template and started calculating screen width

--- src/js/post-image.js
+++ src/js/post-image.js
@@ -4,4 +4,10 @@
  * When a post has a post image, we need to resize the containing div to span the entire available area.
  * That way, the image is nicely stretched across the page.
  */
-
+(function ($) {
+    var postImageWrapper = $(".js-post-image-wrapper")[0];
+    var sidebar = $(".js-sidebar")[0];
+    var availableScreenWidth = $(window).innerWidth();
+    
+    
+})($);
--- src/js/post-image.spec.js
+++ src/js/post-image.spec.js
@@ -2,8 +2,4 @@ describe("Test", function () {
     it("should pass this test", function () {
         expect(true).toBe(true);
     });
-
-    it("should fail this test", function () {
-        expect(true).tobe(false);
-    });
 });
