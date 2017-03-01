fdd1615e10cd925a7f52eadb43c4197a1c1faa93
Lody Borgers <hi@lodybo.nl>
2017-03-01 21:43:39 +0100

    Fixed the post image scaling for ya

--- src/js/post-image.js
+++ src/js/post-image.js
@@ -5,21 +5,23 @@
  * That way, the image is nicely stretched across the page.
  */
 (function ($) {
-    if ($(".js-post-image-wrapper").length) {
+    $(window).on("load", function () {
+        if ($(".js-post-image-wrapper").length) {   
+            $(window).resize(resizePostImage);
+            resizePostImage();
+        }
+    });
+    
+    function resizePostImage() {
         var postImageWrapper = $(".js-post-image-wrapper");
         var sidebar = $(".js-sidebar");
         var $window = $(window);
 
-        resizePostImage();        
-        window.addEventListener("resize", resizePostImage);
+        // The "+1" is there because the sidebar width is calculated using percentages. jQuery returns the width as a rounded-down number, so we'll round up manually.
+        var sidebarWidth = sidebar.innerWidth() + 1;
+        var windowWidth = $window.innerWidth();
+
+        var availableScreenWidth = windowWidth - sidebarWidth;
+        postImageWrapper.width(availableScreenWidth);
     }
-
-    function resizePostImage() {
-            // The "-1" is there because the sidebar width is calculated using percentages. jQuery returns the width as a rounded-down number, so we'll round up manually.
-            var sidebarWidth = sidebar.innerWidth() + 1;
-            var windowWidth = $window.innerWidth();
-
-            var availableScreenWidth = windowWidth - sidebarWidth;
-            postImageWrapper.width(availableScreenWidth);
-        }
 })($);
