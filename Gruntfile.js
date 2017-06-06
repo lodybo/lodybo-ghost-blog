ff0dc1350b0acac6062048fb29b06fb8c1ecce70
Lody Borgers <hi@lodybo.nl>
2017-06-06 23:22:23 +0200

    Started the restructure in removing the Ghocustom Gt as asset, in preference to Docker

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
 			},
 			dist: {
 				src: ["src/scss/lodybo.scss"],
-				dest: "dist/content/themes/lodybo-theme/assets/css/styles.css"
+				dest: "dist/lodybo-theme/assets/css/styles.css"
 			}
 		},
 		postcss: {
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
 					sourceMap: true
 				},
 				files: {
-					"dist/content/themes/lodybo-theme/assets/js/scripts.min.js": ["src/js/*.js", "!src/js/*.spec.js"]
+					"dist/lodybo-theme/assets/js/scripts.min.js": ["src/js/*.js", "!src/js/*.spec.js"]
 				}
 			},
 			dev: {
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
 					beautify: true
 				},
 				files: {
-					"dist/content/themes/lodybo-theme/assets/js/scripts.min.js": ["src/js/*.js", "!src/js/*.spec.js"]
+					"dist/lodybo-theme/assets/js/scripts.min.js": ["src/js/*.js", "!src/js/*.spec.js"]
 				}
 			}
 		},
@@ -107,22 +107,15 @@ module.exports = function (grunt) {
 			dist: "dist",
 			build: "build",
 			sassdoc: "docs/sassdoc",
-			jsdoc: "docs/jsdoc",
-			ghost: "assets/heroku-preconfigured-ghost",
+			jsdoc: "docs/jsdoc"
 		},
 		copy: {
-			ghost: {
-				expand: true,
-				cwd: "assets/heroku-preconfigured-ghost/ghost",
-				src: "**",
-				dest: "dist/"
-			},
 			images: {
 				files: [{
 					expand: true,
 					flatten: true,
 					src: ["assets/images/**/*.jpg", "assets/images/**/***/.jpeg", "assets/images/**/*.png"],
-					dest: "dist/content/themes/lodybo-theme/assets/images"
+					dest: "dist/lodybo-theme/assets/images"
 				}]
 			},
 			templates: {
@@ -131,20 +124,9 @@ module.exports = function (grunt) {
 						expand: true,
 						flatten: true,
 						src: ["src/templates/**/*.hbs"],
-						dest: "dist/content/themes/lodybo-theme"
+						dest: "dist/lodybo-theme"
 					}
 				]
-			}
-		},
-		shell: {
-			"pull-ghost": {
-				command: "git clone https://github.com/lodybo/lodybo-ghost.git assets/heroku-preconfigured-ghost"
-			},
-			"install-ghost": {
-				command: ["cd dist/", "npm install --production"].join("&&")
-			},
-			"run-ghost": {
-				command: ["cd dist/", "npm start"].join("&&")
 			}
 		},
 		watch: {
@@ -163,48 +145,12 @@ module.exports = function (grunt) {
 				files: ["src/js/**/*.js"],
 				tasks: ["eslint:dev" /*, "karma:unit"*/ , "uglify:dev"]
 			}
-		},
-		concurrent: {
-			options: {
-                logConcurrentOutput: true
-            },
-			dev: ["shell:run-ghost", "serve"]
 		}
 	});
 
 	require('load-grunt-tasks')(grunt);
 
 	// ***** GRUNT TASKS
-	grunt.registerTask("setup-ghost", function (run) {
-		// ["clean:dist", "clean:ghost", "shell:pull-ghost", "copy:ghost", "shell:install-ghost"]
-
-		console.log("Setting up Ghost..");
-
-		// First clean the dist and ghost folders
-		console.log("* Cleaning the dist/ and ghost/ folders");
-		grunt.task.run(["clean:dist", "clean:ghost"]);
-
-		// Pull ghost from server
-		console.log("* Pulling Heroku-preconfigured Ghost instance from repo");
-		grunt.task.run(["shell:pull-ghost"]);
-
-		// Copy Ghost instance to dist folder
-		console.log("* Copy Ghost instance to dist/ folder");
-		grunt.task.run(["copy:ghost"]);
-
-		// Finally, install Ghost
-		console.log("* Running 'npm install --production' in dist/ folder");
-		grunt.task.run(["shell:install-ghost"]);
-
-		// If run
-		if (run) {
-			console.log("Running Ghost locally in dev mode");
-			grunt.task.run(["shell:run-ghost"]);
-		}
-
-		// Done
-		console.log("Done setting up Ghost..");
-	});
 
 	grunt.registerTask("create-theme-package", function () {
 		var pkg = grunt.file.readJSON("package.json");
@@ -213,7 +159,7 @@ module.exports = function (grunt) {
 			version: pkg.version
 		};
 
-		grunt.file.write("dist/content/themes/lodybo-theme/package.json", JSON.stringify(themePackage));
+		grunt.file.write("dist/lodybo-theme/package.json", JSON.stringify(themePackage));
 		console.log("Written theme package.json for Lodybo Ghost Theme");
 		console.log("- Theme name: ", themePackage.name);
 		console.log("- Theme version: ", themePackage.version);
@@ -248,6 +194,6 @@ module.exports = function (grunt) {
 	grunt.registerTask("test", ["eslint:production" /*, "karma:unit"*/]);
 	grunt.registerTask("build", ["clean:build", "scss", "js", "copy:images", "copy:templates", "create-theme-package"]);
 
-	grunt.registerTask("default", ["concurrent:dev"]);
+	grunt.registerTask("default", ["serve"]);
 
 };
--- README.md
+++ README.md
@@ -3,16 +3,10 @@
 This repo contains the source code of my Ghost blog instance + theme.
 It uses a Heroku pre-configured instance of Ghost, obtainable from [this repo](https://github.com/lodybo/lodybo-ghost).
 
-## First time setup
-After checking out this project and doing `npm install`, we'll need to locally fetch the source from the pre-configured Ghost instance. 
-Grunt will do this for us, via the `grunt setup-ghost` command. You can even specify if you want Ghost to be running immediately by passing the `run` parameter: `grunt setup-ghost:run`. 
-Ghost will be downloaded into the `assets/` directory. It will also be copied to the `dist/` folder, which we'll use for serving the theme.
-**Warning:** Ghost runs on NodeJS versions 0.10.0, 0.12.0, and 4.2.0 - 5.0.0.
+## Prerequisites
+Ghost needs to be running during development. This can be done by using the Docker Toolbox, and mount a volume from Docker to the `dist/` folder.
 
 ## Development
-Ghost needs to run while developing, that can be done with the `grunt shell:run-ghost` command. Visit your fresh Ghost install on `localhost:2368`. 
-After that, `grunt serve` will watch when you develop any source files.
+`grunt serve` will watch when you develop any source files.
 
-Alternatively, you can run `grunt` or `grunt start` and it will run the two tasks defined above.
-
-Also, `grunt test` will run all the tests and `grunt build` will build build everything and move it to the `dist/` folder where we can preview.
+Also, `grunt test` will run all the tests and `grunt build` will build build everything and move it to the `dist/` folder where we can preview. This will create a theme package that you can upload to a Ghost server.
